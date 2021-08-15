4eaaca372d1c29488075acf851bd9666ac5d66f0
Luis Genesius <luis.huang321@gmail.com>
2021-08-15 13:09:07 +0700

    reinit xcode project

---
+++ GameList/GameList/GameListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameListApp.swift
+//  GameList
+//
+//  Created by Luis Genesius on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GameListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
