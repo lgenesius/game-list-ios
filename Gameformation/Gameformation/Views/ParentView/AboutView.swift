a43bb031a41ddc76578db108231d63c600545c0a
Luis Genesius <luis.huang321@gmail.com>
2021-08-16 14:54:04 +0700

    about view finish

--- Gameformation/Gameformation/Views/ParentView/AboutView.swift
+++ Gameformation/Gameformation/Views/ParentView/AboutView.swift
@@ -8,10 +8,65 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    private let myBackground = "Saya adalah mahasiswa universitas Bina Nusantara yang mengambil jurusan Teknik Informatika."
+    private let myPurpose = "Memiliki tujuan untuk menjadi seorang iOS Developer yang handal."
+    private let myPassion = "Saya memiliki passion untuk mengembangkan aplikasi karena"
+    private let myReason = "dapat menghasilkan sebuah produk yang bisa memberikan manfaat bagi banyak orang."
+    
     var body: some View {
         NavigationView {
-            Text("Hello")
-                .navigationBarTitle("About")
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2274509804, green: 0.3803921569, blue: 0.5254901961, alpha: 1)), Color(#colorLiteral(red: 0.537254902, green: 0.1450980392, blue: 0.2431372549, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                ScrollView {
+                    LazyVStack {
+                        VStack {
+                            Image("me")
+                                .circularImageStyle()
+                                .padding(.top, 20)
+                            
+                            Text("Luis Genesius")
+                                .bigTitleStyle()
+                                .padding(.top, 15)
+                            
+                            Text("Singkawang, Kalimantan Barat")
+                                .titleCardStyle()
+                                .padding(.top, 1)
+                            
+                            VStack {
+                                
+                                HStack {
+                                    Text("Tentang Saya")
+                                        .titleCardStyle()
+                                        .padding(.horizontal)
+                                        .padding(.top, 20)
+                                        .padding(.bottom, 5)
+                                    Spacer()
+                                }
+                                HStack {
+                                    Text(myBackground + " " + myPurpose)
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal)
+                                    Spacer()
+                                }
+                                
+                                HStack {
+                                    Text(myPassion + " " + myReason)
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal)
+                                        .padding(.top, 5)
+                                    Spacer()
+                                }
+                            }
+                            
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("About Me")
         }
     }
 }
--- Gameformation/Gameformation/Views/ViewExtension/View+Extension.swift
+++ Gameformation/Gameformation/Views/ViewExtension/View+Extension.swift
@@ -13,7 +13,22 @@ extension View {
         self.modifier(TextModifier(color: .white, size: 15, weight: .regular))
     }
     
+    func bigTitleStyle() -> some View {
+        self.modifier(TextModifier(color: .white, size: 25, weight: .semibold))
+    }
+    
     func roundedEdgeCard() -> some View {
         self.modifier(RoundedEdge(width: 2, cornerRadius: 10))
+    }
+}
+
+extension Image {
+    func circularImageStyle() -> some View {
+        self
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipShape(Circle())
+            .shadow(radius: 10)
     }
 }
