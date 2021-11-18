75026bd821d8b06d841c4bb54a19de286ba50b47
Luis Genesius <luis.huang321@gmail.com>
2021-11-18 14:20:08 +0700

    replace info.plist to the original place, add api struct

--- Gameformation/Gameformation/Core/Utils/Network/APIEndpoints.swift
+++ Gameformation/Gameformation/Core/Utils/Network/APIEndpoints.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct API {
+    
+    static let baseURL = "https://api.rawg.io/api"
+    static var key: String {
+        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+            fatalError("Couldn't find key 'API_KEY' in 'Info.plist'.")
+        }
+        
+        return apiKey
+    }
+    
+    enum Endpoint {
+        case games
+        case game(id: Int)
+        case search(query: String)
+        
+        var urlString: String {
+            switch self {
+            case .games:
+                return "\(API.baseURL)/games?key=\(API.key)"
+            case .game(let id):
+                return "\(API.baseURL)/games/\(id)?key=\(API.key)"
+            case .search(let query):
+                return "\(API.baseURL)/games/?key=\(API.key)&search=\(query)"
+            }
+        }
+    }
+}
