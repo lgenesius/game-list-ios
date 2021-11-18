0c1c68758fcce65a6e96292f850b29689499052d
Luis Genesius <luis.huang321@gmail.com>
2021-11-18 17:05:14 +0700

    add functions to gamelocaldatasource

--- Gameformation/Gameformation/Core/Data/Local/GameLocalDataSource.swift
+++ Gameformation/Gameformation/Core/Data/Local/GameLocalDataSource.swift
@@ -6,3 +6,96 @@
 //
 
 import Foundation
+import CoreData
+import Combine
+
+protocol GameLocalDataSourceProtocol {
+    
+    func getGames() -> AnyPublisher<[GameEntity], Error>
+    func getGame(with id: Int) -> AnyPublisher<GameEntity?, Error>
+    func addGame(id: Int, name: String, released: String?, overallRating: Double, backgroundImage: String?) -> AnyPublisher<Bool, Never>
+    func deleteGame(game: GameEntity) -> AnyPublisher<Bool, Never>
+}
+
+final class GameLocalDataSource {
+    static let shared = GameLocalDataSource()
+    
+    private init() {}
+}
+
+extension GameLocalDataSource: GameLocalDataSourceProtocol {
+    
+    func getGames() -> AnyPublisher<[GameEntity], Error> {
+        return Future<[GameEntity], Error> { completion in
+            let fetchRequest: NSFetchRequest<GameEntity> = GameEntity.fetchRequest()
+            
+            do {
+                let games = try PersistentContainer.viewContext.fetch(fetchRequest)
+                completion(.success(games))
+            } catch {
+                PersistentContainer.viewContext.rollback()
+                completion(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func getGame(with id: Int) -> AnyPublisher<GameEntity?, Error> {
+        return Future<GameEntity?, Error> { completion in
+            let fetchRequest: NSFetchRequest<GameEntity> = GameEntity.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "id == %d", Int32(id))
+            
+            do {
+                let games = try PersistentContainer.viewContext.fetch(fetchRequest)
+                completion(.success(games.first))
+            } catch {
+                PersistentContainer.viewContext.rollback()
+                completion(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func addGame(id: Int, name: String, released: String?, overallRating: Double, backgroundImage: String?) -> AnyPublisher<Bool, Never> {
+        return Future<Bool, Never> { [weak self] completion in
+            let game = GameEntity(context: PersistentContainer.viewContext)
+            game.id = Int32(id)
+            game.name = name
+            game.released = released
+            game.overallRating = overallRating
+            game.backgroundImage = backgroundImage
+            
+            do {
+                try self?.save()
+                completion(.success(true))
+            } catch {
+                PersistentContainer.viewContext.rollback()
+                completion(.success(false))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func deleteGame(game: GameEntity) -> AnyPublisher<Bool, Never> {
+        return Future<Bool, Never> { [weak self] completion in
+            PersistentContainer.viewContext.delete(game)
+            
+            do {
+                try self?.save()
+                completion(.success(true))
+            } catch {
+                PersistentContainer.viewContext.rollback()
+                completion(.success(false))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    private func save() throws {
+        do {
+            try PersistentContainer.viewContext.save()
+        } catch {
+            throw(error)
+        }
+    }
+}
