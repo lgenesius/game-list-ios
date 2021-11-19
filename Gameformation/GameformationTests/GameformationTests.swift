7791fc8cf6d1df748878bcbe07b7d12695fae1ce
Luis Genesius <luis.huang321@gmail.com>
2021-11-19 22:26:53 +0700

    add unit test

---
+++ Gameformation/GameformationTests/GameformationTests.swift
@@ -0,0 +1,32 @@
+//
+//  GameformationTests.swift
+//  GameformationTests
+//
+//  Created by Luis Genesius on 19/11/21.
+//
+
+import XCTest
+
+class GameformationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
