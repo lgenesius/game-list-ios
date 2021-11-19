f81eea2faeb3d1a96de62de9a53742567e29cd8a
Luis Genesius <luis.huang321@gmail.com>
2021-11-19 23:04:08 +0700

    add home interactor unit tests

---
+++ Gameformation/GameformationTests/UseCaseTests/HomeInteractorTests.swift
@@ -0,0 +1,129 @@
+//
+//  HomeInteractorTests.swift
+//  GameformationTests
+//
+//  Created by Luis Genesius on 19/11/21.
+//
+
+import XCTest
+import Combine
+@testable import Gameformation
+
+extension Game: Equatable {
+    public static func == (lhs: Game, rhs: Game) -> Bool {
+        true
+    }
+}
+
+class HomeInteractorTests: XCTestCase {
+    private let homeUseCase = Injection().provideHomeUseCase()
+    
+    private var cancelables: Set<AnyCancellable> = []
+    
+    func testGetGames() {
+        let expectation = self.expectation(description: "GetGames")
+        
+        var error: Error?
+        var games: [Game] = []
+        var nextPage: String?
+        
+        homeUseCase.getGames()
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let encountedError):
+                    error = encountedError
+                }
+                
+                expectation.fulfill()
+            } receiveValue: { values in
+                nextPage = values.0
+                games = values.1
+            }
+            .store(in: &cancelables)
+        
+        waitForExpectations(timeout: 5)
+        
+        XCTAssertNil(error)
+        XCTAssertNotNil(nextPage)
+        XCTAssertNotEqual([], games)
+        
+        XCTAssertNotNil(URL(string: nextPage!))
+    }
+    
+    func testSearchGames() {
+        let expectation = self.expectation(description: "SearchGames")
+        
+        var error: Error?
+        var games: [Game] = []
+        var nextPage: String?
+        
+        let query = "Dota"
+        homeUseCase.searchGame(query: query)
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let encountedError):
+                    error = encountedError
+                }
+                
+                expectation.fulfill()
+            } receiveValue: { values in
+                nextPage = values.0
+                games = values.1
+            }
+            .store(in: &cancelables)
+        
+        waitForExpectations(timeout: 5)
+        
+        XCTAssertNil(error)
+        XCTAssertNotNil(nextPage)
+        XCTAssertNotEqual([], games)
+        
+        XCTAssertNotNil(URL(string: nextPage!))
+    }
+    
+    func testNextGames() {
+        let expectation = self.expectation(description: "NextGames")
+        
+        var error: Error?
+        var games: [Game] = []
+        var nextPage: String?
+        
+        let urlString = API.Endpoint.search(query: "Dota").urlString + "&page=2"
+        let url = URL(string: urlString)
+        XCTAssertNotNil(url)
+        
+        homeUseCase.nextGames(url: url!)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let encountedError):
+                    error = encountedError
+                }
+                
+                expectation.fulfill()
+            } receiveValue: { values in
+                nextPage = values.0
+                games = values.1
+            }
+            .store(in: &cancelables)
+        
+        waitForExpectations(timeout: 5)
+        
+        XCTAssertNil(error)
+        XCTAssertNotNil(nextPage)
+        XCTAssertNotEqual([], games)
+        
+        XCTAssertNotNil(URL(string: nextPage!))
+    }
+    
+    deinit {
+        cancelables.removeAll()
+    }
+}
