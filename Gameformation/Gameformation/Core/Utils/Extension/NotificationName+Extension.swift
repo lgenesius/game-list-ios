82717cce963cd91a60ef5990f49b65994a8c2c44
Luis Genesius <luis.huang321@gmail.com>
2021-12-04 19:34:14 +0700

    change gameformation tests ios deployment target and delete module in gameformation

--- Gameformation/Gameformation/Core/Utils/Extension/NotificationName+Extension.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension Notification.Name {
-    static let refreshFavorite = Notification.Name("refreshFavorite")
-    static let refreshAbout = Notification.Name("refreshAbout")
-}
