cc2409305c66893039112fa0e06d6890d0be4a62
Luis Genesius <luis.huang321@gmail.com>
2021-12-06 06:43:21 +0700

    delete comment in injection

--- Gameformation/Gameformation/Core/DI/Injection.swift
+++ Gameformation/Gameformation/Core/DI/Injection.swift
@@ -12,34 +12,7 @@ import GamePackage
 import DetailGame
 import Favorite
 
-// swiftlint:disable all
 final class Injection {
-    
-//    private func provideGameRemoteRepository() -> GameRemoteRepositoryProtocol {
-//        let remote = GameRemoteDataSource.shared
-//        return GameRemoteRepository.shared(remote)
-//    }
-//
-//    private func provideGameLocalRepository() -> GameLocalRepositoryProtocol {
-//        let local = GameLocalDataSource.shared
-//        return GameLocalRepository.shared(local)
-//    }
-    
-//    func provideHomeUseCase() -> HomeUseCase {
-//        let repository = provideGameRemoteRepository()
-//        return HomeInteractor(repository: repository)
-//    }
-    
-//    func provideDetailUseCase(id: Int) -> DetailUseCase {
-//        let remote = provideGameRemoteRepository()
-//        let local = provideGameLocalRepository()
-//        return DetailInteractor(id: id, remoteRepository: remote, localRepository: local)
-//    }
-//
-//    func provideFavoriteUseCase() -> FavoriteUseCase {
-//        let local = provideGameLocalRepository()
-//        return FavoriteInteractor(localRepository: local)
-//    }
     
     func provideHomeUseCase<U: HomeUseCase>() -> U where U.Request == Any, U.Response == GameModel {
         let remoteDataSource = GameRemoteDataSource()
